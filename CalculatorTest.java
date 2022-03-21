c589a81851dce077a149652b69e147077762493a
luisaruiz0 <98635984+luisaruiz0@users.noreply.github.com>
2022-03-21 12:05:23 -0700

    Update CalculatorTest.java

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -1,4 +1,13 @@
-public class CalculatorTest
-{
-    
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void theEvaluatePostfix1() {
+        Calculator calc = new Calculator();
+        String string = "23*42-/56*+";
+        assertEquals(33, calc.evaluatePostfix(string));
+    }
 }
