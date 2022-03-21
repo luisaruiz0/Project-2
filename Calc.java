b5a33c765c69e221619a61cc0337f41e1d5a1574
sharyrazo <98670076+sharyrazo@users.noreply.github.com>
2022-03-21 09:18:13 -0700

    conv postfix and the demo bit

--- Calc.java
+++ Calc.java
@@ -45,10 +45,40 @@ public class Calc {
         return valueStack.peek();
 	}
 	
+	public static String convertToPostfix(String infix) throws Exception {
+	   StackInterface<Character> operatorStack = new LinkedStack<Character>();//empty stack  
+	   String postfix = ""; //empty string
+	   infix = infix + ")";
+	   operatorStack.push('(');	      
+	      for(int i=0; i<infix.length(); i++){ //converting from infix to post	    	  
+	         char exp = infix.charAt(i);	          
+	            if(Character.isLetter(exp)){
+	               postfix = postfix + exp + " ";
+	            }
+	            else if(exp=='('){//checks parentheses
+	            	operatorStack.push(exp);
+	            }
+	            else if(exp==')'){
+	               while(operatorStack.peek()!='('){
+	                  postfix = postfix + operatorStack.peek() + " ";
+	                  operatorStack.pop();
+	               }
+	               operatorStack.pop();
+	            }
+	       }	      
+	       if(!operatorStack.isEmpty())
+	           System.out.println("Not valid please enter correctly");	  
+	       return postfix;
+	   }
+	
 	/** Main method to test convertToPostfix and evaluatePostfix method */
-	public static void main(String args[]) {
-		String string1 = "23*42-/56*+";
-		System.out.println(evaluatePostfix(string1));
+         public static void main(String args[])throws Exception {
+            String infix = "a*b/(c-a)+d*e"; //use string from assignment
+	    String postfix = convertToPostfix(infix);
+	    System.out.println ("postfix exp: " + postfix);
+		
+	    String string1 = "23*42-/56*+";
+            System.out.println(evaluatePostfix(string1));
 	}
 	
 }
