a8c87835d6b1a72283b7e37e69e3d6f30f01205a
Guillermo Roman <roman.guillermo52@gmail.com>
2022-03-21 11:31:53 -0700

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -7,6 +7,33 @@
  */
 
 public class Calc {
+	
+	public static String convertToPostfix(String infix) throws Exception {
+	   StackInterface<Character> operatorStack = new LinkedStack<Character>();//empty stack  
+	   String postfix = ""; //empty string
+	   infix = infix + ")";
+	   operatorStack.push('(');	      
+	      for(int i=0; i<infix.length(); i++){ //converting from infix to post	    	  
+	         char exp = infix.charAt(i);	          
+	            if(Character.isLetter(exp)){
+	               postfix = postfix + exp + " ";
+	            }
+	            else if(exp=='('){//checks parentheses
+	            	operatorStack.push(exp);
+	            }
+	            else if(exp==')'){
+	               while(operatorStack.peek()!='('){
+	                  postfix = postfix + operatorStack.peek() + " ";
+	                  operatorStack.pop();
+	               }
+	               operatorStack.pop();
+	            }
+	       }	      
+	       if(!operatorStack.isEmpty())
+	           System.out.println("Not valid please enter correctly");	  
+	       return postfix;
+	   }
+	
 	/**
 	 * Method to evaluate postfix expression
 	 * @param postfix expression to evaluate, given as a string of characters
@@ -45,40 +72,12 @@ public class Calc {
         return valueStack.peek();
 	}
 	
-	public static String convertToPostfix(String infix) throws Exception {
-	   StackInterface<Character> operatorStack = new LinkedStack<Character>();//empty stack  
-	   String postfix = ""; //empty string
-	   infix = infix + ")";
-	   operatorStack.push('(');	      
-	      for(int i=0; i<infix.length(); i++){ //converting from infix to post	    	  
-	         char exp = infix.charAt(i);	          
-	            if(Character.isLetter(exp)){
-	               postfix = postfix + exp + " ";
-	            }
-	            else if(exp=='('){//checks parentheses
-	            	operatorStack.push(exp);
-	            }
-	            else if(exp==')'){
-	               while(operatorStack.peek()!='('){
-	                  postfix = postfix + operatorStack.peek() + " ";
-	                  operatorStack.pop();
-	               }
-	               operatorStack.pop();
-	            }
-	       }	      
-	       if(!operatorStack.isEmpty())
-	           System.out.println("Not valid please enter correctly");	  
-	       return postfix;
-	   }
-	
 	/** Main method to test convertToPostfix and evaluatePostfix method */
          public static void main(String args[])throws Exception {
             String infix = "a*b/(c-a)+d*e"; //use string from assignment
 	    String postfix = convertToPostfix(infix);
 	    System.out.println ("postfix exp: " + postfix);
-		
-	    String string1 = "23*42-/56*+";
-            System.out.println(evaluatePostfix(string1));
+            System.out.println(evaluatePostfix(postfix)); //evaluate postfix string result from above
 	}
 	
 }
