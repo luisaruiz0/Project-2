41b04cfd6e8db0c3ae6e25dce6e15a904ee42794
Guillermo Roman <roman.guillermo52@gmail.com>
2022-03-21 21:08:44 -0700

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -8,31 +8,65 @@
 
 public class Calculator {
 	
-	public static String convertToPostfix(String infix) throws Exception {
-	   StackInterface<Character> operatorStack = new LinkedStack<Character>();//empty stack  
-	   String postfix = ""; //empty string
-	   infix = infix + ")";
-	   operatorStack.push('(');	      
-	      for(int i=0; i<infix.length(); i++){ //converting from infix to post	    	  
-	         char exp = infix.charAt(i);	          
-	            if(Character.isLetter(exp)){
-	               postfix = postfix + exp + " ";
-	            }
-	            else if(exp=='('){//checks parentheses
-	            	operatorStack.push(exp);
-	            }
-	            else if(exp==')'){
-	               while(operatorStack.peek()!='('){
-	                  postfix = postfix + operatorStack.peek() + " ";
-	                  operatorStack.pop();
+	/**
+	 * Method to convert an infix expression to postfix
+	 * @param infix expression (string) to convert
+	 * @return Resulting postfix expression 
+	 */
+	 public static String convertToPostfix(String infix) {
+	       infix += ")";
+	       StackInterface<Character> operatorStack = new LinkedStack<Character>();
+	       operatorStack.push('(');
+	       String postfix = "";
+	       //convert from infix to postfix
+	       for(int i=0; i<infix.length(); i++) {
+	           char ch = infix.charAt(i);
+	           //check for variable
+	           if(Character.isLetter(ch)) {
+	               postfix = postfix + ch + " ";
+	           }
+	           //check for left parenthesis
+	           else if(ch=='(') {
+	               operatorStack.push(ch);
+	           }
+	           //check for righr parenthesis
+	           else if(ch==')') {
+	               while(operatorStack.peek()!='(') {
+	                   postfix = postfix + operatorStack.peek() + " ";
+	                   operatorStack.pop();
 	               }
 	               operatorStack.pop();
-	            }
-	       }	      
-	       if(!operatorStack.isEmpty())
-	           System.out.println("Not valid please enter correctly");	  
+	           }
+	           //operator
+	           else {
+	               int p1 = priorityOperator(ch);
+	               int p2 = priorityOperator(operatorStack.peek());
+	               while(p1<=p2) {
+	                   postfix = postfix + operatorStack.peek() + " ";
+	                   operatorStack.pop();
+	                   p2 = priorityOperator(operatorStack.peek());
+	               }
+	               operatorStack.push(ch);
+	           }
+	       }
 	       return postfix;
-	   }
+	 }
+	 
+	 private static int priorityOperator(char c) { // Method for order of operations
+	       switch(c)
+	       {
+	           case '(': 
+	        	   return 0;
+	           case '/': 
+	           case '*': 
+	        	   return 2;
+	           case '+': 
+	           case '-': 
+	        	   return 1;
+	           default: 
+	        	   return 999;
+	       }
+	 }
 	
 	/**
 	 * Method to evaluate postfix expression
